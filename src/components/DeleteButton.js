f37016881ce878457804c80697b284a3a6dca27e
Shir Koren <shirko6@gmail.com>
2022-09-05 15:00:08 +0200

    delete function

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -1,6 +1,27 @@
-export default function DeleteButton() {
 
+
+//let ToDoDta should be state with const
+const [toDoData, setToDoData] = useState([{ id: 1, text: 'textData', done: true},{id: 2, text: 'textData2', done: false}])
+//this function needs to be in app.js:
+const handleDeleteTask = (id) =>{
+       
+    const newToDoData = toDoData.filter((eachToDo) =>{ eachToDo.id !== id
+
+   })
+  setToDoData(newToDoData)
+}
+//in the ListItem.js:
+<DeleteButton id={key}/>
+
+//this can be the repalcment to the function handleNewTaskClick (line 24) in ToDo.js if we make the ToDoData as a state.
+setToDoData((prev) => {[ ...prev, {id: toDoData[toDoData.length-1], ...newData } ]})
+setToDoData(newData)
+
+//this sateyd here:
+export default function DeleteButton({id}) {    
+    
     return (
-        <button>Delete</button>
+        <button onClick={()=>handleDeleteTask(id)} >Delete</button>
     )
 }
+
